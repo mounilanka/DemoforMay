03a99aa344ec73af59f6753be2eaf369f6cda90c
mounilanka <mounikalanka989@gmail.com>
2022-06-13 22:41:46 +0530

    initial commit first

---
+++ testing.py
@@ -0,0 +1,11 @@
+from calender import c
+from pakage import calculator
+
+cal = calculator.Calci(
+     a = 30,
+     b = 40
+)
+
+print(f"This is addded value {cal.addition()}")
+print(f"This is subracted value {cal.subraction()}")
+
